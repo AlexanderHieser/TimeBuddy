219a18eca83902156f64a8e9556a4a32e7957272
Alexander Hieser <Alexander.Hieser@online.de>
2018-02-24 21:29:19 +0100

    create dashboard and options page, delete autogenerated pages. First attempt to create layout

---
+++ src/pages/dashboard/dashboard.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import * as moment from 'moment'
+/**
+ * Generated class for the DashboardPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-dashboard',
+  templateUrl: 'dashboard.html',
+})
+export class DashboardPage {
+
+  date:string = "Hello";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.date = moment().format("DD.MM.YYYY");
+    console.log('ionViewDidLoad DashboardPage');
+  }
+
+}
